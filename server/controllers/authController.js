dd7de4a79d1930f94525b2a7e9d5e88c155b4bdc
shanabbas <shankhanbkr@gmail.com>
2023-10-10 13:24:31 +0500

    Completed User Module

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,3 +1,76 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from './../models/UserModel';
+import User from './../models/UserModel.js';
+
+//VERIFY TOKEN
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header('Authorization');
+
+    if (!token) {
+      return res.status(403).send('Access Denied');
+    }
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+// REGISTER USER
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// LOGGING IN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ status: 'fail', message: 'User does not exist.' });
+
+    const isMatch = await bcrypt.compare(password, userpasssword);
+    if (!isMatch)
+      return res.status(400).json({ message: 'Invalid Credentials.' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+  } catch (err) {}
+};
--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-import {register} from 
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import authRoutes from './routes/authRoute.js';
+import userRoutes from './routes/userRoute.js';
+import { register } from './controllers/authController.js';
 
 // CONFIGURATIONS
 
@@ -20,24 +23,19 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   helmet.crossOriginResourcePolicy({
-    policy: "cross-origin",
+    policy: 'cross-origin',
   })
 );
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(
-  bodyParser.urlencoded({ limit: "30mb", extended: true })
-);
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.use(
-  "/assets",
-  express.static(path.join(__dirname, "public/assets"))
-);
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 // FILE STORAGE
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/assets");
+    cb(null, 'public/assets');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -46,12 +44,15 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // ROUTES WITH FILES
-app.post("/auth/resgister",upload.single('picture'),register);
+app.post('/auth/register', upload.single('picture'), register);
 
+//ROUTES
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 // MONFOOSE SETUP
 const PORT = process.env.PORT || 6001;
 const DB = process.env.MONGO_URL.replace(
-  "<PASSWORD>",
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
@@ -61,9 +62,7 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(PORT, () =>
-      console.log(`Server is running on port ${PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   })
   .catch((error) => {
     console.log(`${error} did not connect`);
